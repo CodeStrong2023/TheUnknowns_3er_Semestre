34435bf35b26d27a9b9677e3ee66ded7afe15179
LautaroRavanal <lrmystgungft@gmail.com>
2024-04-29 22:29:20 -0300

    Java - Video1, video2, video3

---
+++ Java/Leccion01/MundoPc/src/ar/com/system2023/mundopc/DispositivoEntrada.java
@@ -0,0 +1,38 @@
+package ar.com.system2023.mundopc;
+
+public class DispositivoEntrada {
+    private String  tipoEntrada;
+    private String marca;
+    
+public DispositivoEntrada(String tipoEntrada, String marca){
+    this.tipoEntrada = tipoEntrada;
+    this.marca= marca;   
+}    
+
+    public String getTipoEntrada() {
+        return this.tipoEntrada;
+    }
+
+    public void setTipoEntrada(String tipoEntrada) {
+        this.tipoEntrada = tipoEntrada;
+    }
+
+    public String getMarca() {
+        return this.marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("DispositivoEntrada{tipoEntrada=").append(tipoEntrada);
+        sb.append(", marca=").append(marca);
+        sb.append('}');
+        return sb.toString();
+    }
+    
+    
+}
