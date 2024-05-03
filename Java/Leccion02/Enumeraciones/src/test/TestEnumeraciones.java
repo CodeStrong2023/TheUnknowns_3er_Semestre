1802cce5c8f60dee3b2380696d8ebef8148e4d85
Juan Belich <jbelich@gmail.com>
2024-05-03 20:18:14 -0300

    Java-Leccion02 Videos 4, 5, 6

--- Java/Leccion02/Enumeraciones/src/test/TestEnumeraciones.java
+++ Java/Leccion02/Enumeraciones/src/test/TestEnumeraciones.java
@@ -14,3 +14,26 @@ public class TestEnumeraciones {
                 System.out.println("No. de paises en el 4to. continente: " + Continentes.AMERICA.getHabitantes());
 
         }
+        
+        private static void indicarDiaSemana(Dias dias){
+        switch(dias){
+                case LUNES -> System.out.println("Primer dia de la semana");
+                
+                case MARTES -> System.out.println("Segundo dia de la semana");
+            
+                case MIERCOLES -> System.out.println("Tercer dia de la semana");  
+                    
+                case JUEVES -> System.out.println("Cuarto dia de la semana");
+                    
+                case VIERNES -> System.out.println("Quinto dia de la semana");
+                    
+                case SABADO -> System.out.println("Sexto dia de la semana");
+                    
+                case DOMINGO -> System.out.println("Septimo dia de la semana");
+                
+                default -> System.out.println("La opcion no corresponde");
+                    
+        }
+    }
+    
+}
