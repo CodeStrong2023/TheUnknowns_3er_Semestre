bdffeef10bf8cd7ff67a8724af1508478d21e8b3
Sol Bilyk <solbilyk14@gmail.com>
2024-05-04 00:28:06 -0300

    Javascript Leccion 3 Videos 6, 7, 8, 9, 10

--- JavaScript/TSLeccion03/01-03-funciones.js
+++ JavaScript/TSLeccion03/01-03-funciones.js
@@ -42,3 +42,55 @@ console.log(miFuncionTexto);
 const sumarFuncionFlecha = (a, b) => a + b ;
 resultado = sumarFuncionFlecha(3,7);
 console.log(resultado);
+
+
+//Funcion de tipo expresion
+let sumar = function(a = 4, b = 8 ){
+    console.log(arguments[0]); //muestra el parametro de a: 
+    console.log(arguments[1]); //muestra el parametro de b:
+    
+    return a + b + arguments[2];
+}
+resultado = sumar(3, 2, 9);
+console.log(resultado); 
+
+//Sumar todos los argumentos
+let respuesta = sumarTodo(5, 4, 13, 10, 9);
+console.log(respuesta);
+
+function sumarTodo(){
+    let suma = 0;
+    for (let i = 0; i < arguments.length ; i++){
+        suma += arguments[i]; //arguments para arreglos
+    }
+    return suma;
+}
+
+
+//Paso por valor - Paso por referencia
+//Tipos primitivos
+
+let k = 10;
+function cambiarValor(a){
+    a = 20;
+}
+
+cambiarValor(k); //k vale 10, recibe el valor a
+console.log(k); //la funcion no afecto al valor de k, porque a fue una variable momentanea (paso por valor)
+
+//Paso por referencia
+//Se debe crear un objeto
+
+const persona = {
+    nombre: 'Juan',
+    apellido: 'Lepez'
+}
+
+console.log(persona)
+function cambiarValorObjeto(p1){
+    p1.nombre = 'Ignacio';
+    p1.apellido = 'Perez';
+}
+
+cambiarValorObjeto(persona);
+console.log(persona);
