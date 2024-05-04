f801989c10943aded78998be769499654e39c15c
ArturoRiera <arturonriera@gmail.com>
2024-05-03 21:14:11 -0300

    JavaScript Leccion01 Video 5,6,7

--- JavaScript/TSLeccion01/01-03-ciclos.js
+++ JavaScript/TSLeccion01/01-03-ciclos.js
@@ -31,3 +31,13 @@ for(let contando = 0 ; contando <= 10; contando++){
     }
 }
 console.log("Termina el ciclo al encontrar el primer numero par")
+
+// palabra reservada continue y etiquetas labels
+inicio:
+for(let contando = 0;contando <= 10;contando++){
+    if(contando % 2 != 0){
+        continue inicio; // continua a la siguiente interacion
+    }
+    console.log(contando);
+}
+console.log("Termina el ciclo");
