f50d9977552f2f154dbaf25b4647677cec6a3545
Juan Belich <jbelich@gmail.com>
2024-05-03 22:23:37 -0300

    Python Leccion09-10 Videos 6, 7, 8, 9

---
+++ Python/Leccion10/MiClase.py
@@ -0,0 +1,30 @@
+class MiClase:
+    #variable de clase, este atributo dara a cada objeto el mismo valor
+    variable_clase = "esta es una variable de clase"
+
+    #la variavle de instancia, da diferentes valores
+    def __init__(self, variable_instancia):
+        self.variable_instancia = variable_instancia
+    #metodos estatico se asocia a la clase
+    @staticmethod
+    def metodo_estatico():
+        print(MiClase.variable_clase)
+
+    @classmethod
+    def metodo_clase(cls):
+        print(cls.variable_clase)
+
+    def metodo_instancia(self):
+        self.metodo_clase()
+        self.metodo_estatico()
+        print(self.variable_clase)
+        print(self.variable_instancia)
+
+print(MiClase.variable_clase)
+miClase1 = MiClase("esta es una variable de instancia")
+print(miClase1.variable_instancia)
+print(miClase1.variable_clase)
+miClase2 = MiClase("esta es otra prueba de variable de instancia")
+print(miClase2.variable_instancia)
+print(miClase2.variable_clase)
+
