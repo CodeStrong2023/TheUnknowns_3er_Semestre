de5baed5a48fd3c4f64b167bb774f04132de6ab2
ArturoRiera <arturonriera@gmail.com>
2024-05-07 22:16:36 -0300

    JavaScript Leccion4 Videos 4, 5

--- JavaScript/TSLeccion04/01-04-objetos.js
+++ JavaScript/TSLeccion04/01-04-objetos.js
@@ -28,3 +28,25 @@ persona2.direccion = 'Salada 14';
 persona2.telefono = '5492618252521';
 console.log(persona2.telefono);
 console.log('Creamos un nuevo objeto');
+
+//Acceder a las propiedades de los objetos
+console.log(persona['apellido']); //Accedemos como si fuera un arreglo
+console.log('Usamos el ciclo for in');
+
+//for in (propiedad es una variable)
+for(propiedad in persona) {
+    console.log(propiedad);
+    console.log(persona[propiedad]);
+}
+console.log('Cambiamos y eliminamos un error');
+
+//Cambiamos dinamicamente el valor de un objeto
+persona.apellido = 'Betancud'; 
+console.log(persona);
+
+// Para agregar una propiedad nueva:
+persona.apellida = 'Otro Apellido';
+console.log(persona);
+
+//Para eliminar una propiedad:
+delete persona.apellida;
