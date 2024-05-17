6ff1b1ae970475f93a4d17e33f0f7cf725b7b9f8
ArturoRiera <arturonriera@gmail.com>
2024-05-16 21:45:18 -0300

    JavaScript Videos 3, 4, 5

--- JavaScript/TSLeccion04/01-04-objetos.js
+++ JavaScript/TSLeccion04/01-04-objetos.js
@@ -87,3 +87,43 @@ function Persona3(nombre,apellido,email){ //Constructor
         return this.nombre+ " "+ this.apellido
     }
 }
+let padre = new Persona3("Leo","Lopez","lopezl@gmail.com");
+padre.nombre = "Luis"; // modificamos el nombre
+padre.telefono = "5492618282821"; // una propiedad exclusiva del objeto padre
+console.log(padre);
+console.log(padre.nombreCompleto()); //utilizamos la funcion
+let madre = new Persona3("Laura","Contrera","contreral@gmail.com");
+console.log(madre);
+console.log(madre.telefono); //la propiedad no esta definida
+console.log(madre.nombreCompleto());
+
+//diferentes formas de crear objetos
+//caso 1
+let miObjeto = new Object(); //esta es una opcion formal
+//caso 2
+let miObjeto2 ={}; //esta opcion es breve y recomendada
+
+//caso string
+let miCadena1 = new String("Hola");//sintaxis formal
+//caso string2 
+let miCadena2 = "Hola"; //esta es la sintaxis simplificada y recomendada
+
+//caso con numeros
+let miNomero = new Number(1); //es formal no recomendable
+//caso con numeros 2
+let miNumero2 = 1;//sintaxis recomendada
+
+//caso boolean 1
+let miBoolean1 = new Boolean(false); //formal
+//caso boolean 2
+let miBoolean2 =false; //sintaxis recomendada
+
+//caso arreglos 1
+let miArreglo1 = new Array(); //formal
+//caso arreglos 2
+let miArreglo2 =[]; //sintaxis recomendada
+
+//caso funcion 1
+let miFuncion1 = new function(){}; // todo despues de new es considerado objeto
+//caso funcion 2
+let miFuncion2 = function(){}; //notacion simplificada y recomendada
