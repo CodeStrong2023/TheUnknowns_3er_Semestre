fe1a645b23c7c9d0be3421f216232b28ae62bcee
ArturoRiera <arturonriera@gmail.com>
2024-05-21 12:08:26 -0300

    JavaScript Clase06 Videos 4, 5, 6

--- JavaScript/TSLeccion05/05-03-clases.js
+++ JavaScript/TSLeccion05/05-03-clases.js
@@ -1,3 +1,6 @@
+// let persona3 = new Persona('Carla','Ponce') NO SE PERMITE USAR LA CLASE SI NO ESTA CREADA
+
+
 class Persona{
     constructor(nombre, apellido){
         this._nombre = nombre;
@@ -6,10 +9,25 @@ class Persona{
     get nombre(){
         return this._nombre;
     }
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+    get apellido(){
+        return this._apellido;
+    }
+
+    set apellido(apellido){
+        this._apellido = apellido;
+    }
 }
 let persona1 = new Persona("Martin", "Perez");
 //console.log(persona1)
 console.log(persona1.nombre);
+persona1.nombre= "Juan Carlos";
+console.log(persona1.nombre)
+
 let persona2 = new Persona("Carlos", "Lara");
-console.log(persona2.nombre)
 //console.log(persona2)
+console.log(persona2.nombre)
+persona2.nombre= "Maria Laura"
+console.log(persona2.nombre)
