a1d1ad0e9b11b7a4e6f05b7574560bfad67dd890
ArturoRiera <arturonriera@gmail.com>
2024-05-21 11:57:08 -0300

    Python Clase6 Videos 5, 6, 7, 8

--- Python/Archivos/Leccion02/Leccion02/leer_archivo.py
+++ Python/Archivos/Leccion02/Leccion02/leer_archivo.py
@@ -1,2 +1,12 @@
-archivo = open('prueba.txt', 'r', encoding='utf8')
-print(archivo.read())
+archivo = open('prueba.txt', 'r', encoding='utf8')# las letras son: "r" read, "a"append, "w"write, "x"
+#print(archivo.read())
+#print(archivo.read(16))
+#print(archivo.read(10)) # continuamos desde la linea anterior
+#print(archivo.readline())
+#print(archivo.readline())
+
+#vamos a iterar el archivo, cada una de las lineas
+#for linea in archivo:
+    #print(linea): iteramos todos los elementos del archivo
+    #print(archivo.readlines()): accedemos al archivo como si fuera una lista
+print(archivo.readlines()[11])
--- Python/Archivos/Leccion02/Leccion02/manejoArchivos.py
+++ Python/Archivos/Leccion02/Leccion02/manejoArchivos.py
@@ -2,7 +2,10 @@ try:
     archivo = open('prueba.txt', 'w', encoding='utf8')  #la W es de write que significa escribir
     archivo.write("Programamos con diferentes tipos de archivos, ahora en txt.\n")  # con .write nos permite escribir dentro del archivo
     archivo.write('Los acentos son iomportantes para las palabras\n')
-    archivo.write('como por ejemplo: acción, ejecición y producción')
+    archivo.write('como por ejemplo: acción, ejecición y producción\n')
+    archivo.write("las letras son:\n r read(leer),\n a append(anexa),\n w write(escribe),\n x(crea un archivo)\n")
+    archivo.write("t text(texto),\n b binary(archivos binarios),\n w+ (lee y escribe a la vez) r+ (son iguales)\n")
+    archivo.write("Saludos a todos los alumnos de la tecnicatura\n")
     archivo.write('con esto terminamos.')
 except Exception as e:
     print(e)
