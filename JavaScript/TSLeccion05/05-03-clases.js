fbaa2beb097efecd714eba37ae699d2aa3a1f53b
LautaroRavanal <lrmystgungft@gmail.com>
2024-05-23 00:56:05 -0300

    Agrego los videos 7, 8 de JS

--- JavaScript/TSLeccion05/05-03-clases.js
+++ JavaScript/TSLeccion05/05-03-clases.js
@@ -20,6 +20,24 @@ class Persona{
         this._apellido = apellido;
     }
 }
+
+class Empleado extends Persona{ //Clase hija
+    constructor(nombre, apellid, departamento){
+        super(nombre, apellido)
+        this._departamento = departamento;
+
+    }
+
+    get departamento(){
+
+    }
+
+    set departamento(departamento){
+        this._departamento = departamento;
+    }
+
+}
+
 let persona1 = new Persona("Martin", "Perez");
 //console.log(persona1)
 console.log(persona1.nombre);
@@ -31,3 +49,7 @@ let persona2 = new Persona("Carlos", "Lara");
 console.log(persona2.nombre)
 persona2.nombre= "Maria Laura"
 console.log(persona2.nombre)
+
+let empleado1 = new Empleado("Maria", "Gimenez", "Sistemas");
+console.log(empleado1);
+console,log(empleado1.nombre);
