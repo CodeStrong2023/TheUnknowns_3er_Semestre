e053d77711b366a071ba577eb7f5d804ff9aaad5
Sol Bilyk <solbilyk14@gmail.com>
2024-05-23 23:09:09 -0300

    Clase 7 Python Videos 3, 4

---
+++ Python/Leccion03/catalogo_peliculas/catalogo_peliculas/test_catalogo_peliculas.py
@@ -0,0 +1,25 @@
+# Creamos el menú
+from dominio.Pelicula import Pelicula
+from servicio.catalogo_peliculas import CatalogoPeliculas as cp
+opcion = None
+while opcion != 4:
+    try:
+        print('Opciones: ')
+        print('1. Agregar Pelicula')
+        print('2. Listar las peliculas')
+        print('3. Eliminar catálogo de peliculas')
+        print('4. Salir')
+        opcion = int(input('Digite una opción de menú (1-4): '))
+        if opcion == 1:
+            nombre_pelicula = input('Digite el nombre de la pelicula: ')
+            pelicula = Pelicula(nombre_pelicula)
+            cp.agregar_peliculas(pelicula)
+        elif opcion == 2:
+            cp.listar_peliculas()
+        elif opcion == 3:
+            cp.eliminar_peliculas()
+    except Exception as e:
+        print(f'Ocurrió un error de tipo_ {e}')
+        opcion = None
+    else:
+        print('Salimos del programa...')
