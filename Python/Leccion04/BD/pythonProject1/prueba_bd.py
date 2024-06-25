b51be1ba0d457d9539848fb2e1ef5d0bbab38807
ArturoRiera <arturonriera@gmail.com>
2024-06-25 18:51:42 -0300

    reparacion archivo

--- Python/Leccion04/BD/pythonProject1/prueba_bd.py
+++ Python/Leccion04/BD/pythonProject1/prueba_bd.py
@@ -6,15 +6,13 @@ conexion = psycopg2.connect(
     host='127.0.0.1',
     port='5432',
     database='test_bd')
-try:
-    with conexion:
-            with conexion.cursor() as cursor:
-                sentencia = 'SELECT * FROM persona WHERE id_persona= %s' #De esta manera ejecutamos la sentencia
-                id_persona = input("Digite el numero para el id_persona: ")
-                cursor.execute(sentencia, (id_persona,))
-                registros = cursor.fetchone() #Recuperamos todos los registros que seran una lista
-                print(registros)
-except Exception as e:
-    print(f"Ocurrio un error {e}")
-finally:
-    conexion.close()
+
+
+cursor = conexion.cursor()
+sentencia = 'SELECT * FROM persona'
+cursor.execute(sentencia) # De esta manera ejecutamos la sentencia
+registros = cursor.fetchall() #Recuperamos todos los registros que seran una lista
+print(registros)
+
+cursor.close()
+conexion.close()
