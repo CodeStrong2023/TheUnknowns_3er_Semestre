a1726017ed63f2301dfc7599fc70a605a1ef9bbb
ArturoRiera <arturonriera@gmail.com>
2024-06-28 11:43:43 -0300

    Java Clase 10 Videos 9,10,11

--- Java/Leccion11/ListarPersona/src/ListadoPersonasApp.java
+++ Java/Leccion11/ListarPersona/src/ListadoPersonasApp.java
@@ -31,7 +31,36 @@ public class ListadoPersonasApp {
         }//Fin del metodo mostrarMenu
 
         private static boolean ejecutarOperacion(Scanner entrada, List<Persona> personas){
-
+            var opcion = Integer.parseInt(entrada.nextLine());
+            var salir = false;
+            // revisamos la opcion digitada a travez de un switch
+            switch (opcion){
+                case 1 ->{//agregar una persona a la lista
+                    System.out.print("digite el nombre: ");
+                    var nombre = entrada.nextLine();
+                    System.out.print("Digite el telefono: ");
+                    var tel = entrada.nextLine();
+                    System.out.print("Digite el correo: ");
+                    var email: entrada.nextLine()
+                    //creamos el objeto persona
+                    var persona = new Persona(nombre, tel, email);
+                    //agregamos la persona a la lista
+                    personas.add(persona);
+                    System.out.println("La lista tiene: "+personas.size()+" elementos");
+                }//Fin del caso 1
+                case 2 ->{//listar a las personas
+                    System.out.println("Listado de personas: ");
+                    //Mejoras con lamda y el metodo de referencia
+                    //personas.forEach((persona) -> System.out.println(persona));
+                    personas.forEach(System.out::println);
+                }// fin caso 2
+                case 3 -> {//salir del ciclo
+                    System.out.println("Hasta pronto...");
+                    salir = true;
+                }//Fin del caso 3
+                default -> System.out.println("Opcion Incorrecta "+opcion);
+            }//Fin del switch
+            return salir;
         }//Fin del metodoejecutarOperacion
 
 }//Fin clase ListadoPersonasApp
