5a21e43ec11444df7e6ae06174176e72828fb734
ArturoRiera <arturonriera@gmail.com>
2024-06-28 13:35:43 -0300

    Clase 11 Python Completa

--- Python/Leccion07/pythonProject/capa_datos_persona/Conexion.py
+++ Python/Leccion07/pythonProject/capa_datos_persona/Conexion.py
@@ -23,3 +23,48 @@ class Conexion:
         conexion = cls.obtenerPool().getconn()
         log.debug(f'Conexion obtenida del pool: {conexion}')
         return conexion
+    @classmethod
+    def obtenerPool(cls):
+        if cls._pool is None:
+            try:
+                cls._pool = pool.SimpleConnectionPool(cls._MIN_CON,
+                                                      cls._MAX_CON,
+                                                      host=cls._HOST,
+                                                      user=cls._USERNAME,
+                                                      password=cls._PASSWORD,
+                                                      port=cls._DB_PORT,
+                                                      database=cls._DATABASE)
+                log.debug(f'Creacion del pool exitosa: {cls._pool}')
+                return cls._pool # Soluciona el error
+            except Exception as e:
+                log.error(f'Ocurrió un error al obtener el pool: {e}')
+                sys.exit()
+        else:
+            return cls._pool
+
+    @classmethod
+    def liberarConexion(cls, conexion):
+        cls.obtenerPool().putconn(conexion)
+        log.debug(f'Regresamos la conexion del pool: {conexion}')
+
+
+    @classmethod
+    def cerrarConexiones(cls):
+        cls.obtenerPool().closeall()
+
+
+#Generamos un prueba de lo anterior
+if __name__ == '__main__':
+    conexion1 = Conexion.obtenerConexion()
+    Conexion.liberarConexion(conexion1)
+
+    conexion2 = Conexion.obtenerConexion()
+    Conexion.liberarConexion(conexion2)
+
+    conexion3 = Conexion.obtenerConexion()
+    Conexion.liberarConexion(conexion3)
+
+    conexion4 = Conexion.obtenerConexion()
+    conexion5 = Conexion.obtenerConexion()
+    conexion6 = Conexion.obtenerConexion()
+
